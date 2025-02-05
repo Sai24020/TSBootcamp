c5124564ddae6bf985d74ae8cc62d9cce301cfdf
Suzan Al Majdalawi <suzanmajdalawi1@gmail.com>
2025-02-05 18:36:06 +0100

    .

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,9 @@
 let greeting: string = "Hello world!";
 console.log(greeting);
  
+let x: string = "42";
+console.log(x);
+
+let name: string = "Alice";
+let age: number = 25;
+let isStudent: boolean = true;
